afd8ff308c81c8b1e4df47d1bcd7b8a4ba746ec6
Ying Liu <ying2.liu@intel.com>
2022-04-12 16:24:40 -0700

    we don't need fork in payload_receiver
    Signed-off-by: Ying Liu <ying2.liu@intel.com>

--- CI-Examples/pal_api/payload_receiver.c
+++ CI-Examples/pal_api/payload_receiver.c
@@ -7,26 +7,16 @@
 #include <unistd.h>
 
 int main(int argc, char** argv) {
-    pid_t child_pid;
-    int newfd = dup(1);
-    if (newfd < 0) {
-        perror("dup failed");
-        return 1;
-    }
-
-    char fd_argv[12];
-    snprintf(fd_argv, 12, "%d", newfd);
-    char* const new_argv[] = {(char*)"./helloworld", fd_argv, NULL};
-    
-    child_pid = fork();
     int flag = 1;
+    char* const new_argv[] = {(char*)"./helloworld", "helloworld", NULL};
 
+    printf("hello from payload_receiver\n");
     while (1) {
-        if ((child_pid == 0) && (flag == 1)){
+        if (flag == 1) {
             execv(new_argv[0], new_argv);
         } 
    }
    
-    puts("test completed successfully");
+    printf("test completed successfully");
     return 0;
 }
